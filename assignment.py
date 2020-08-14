69cdbfc63504559172ad5fc29c2b727b18246e8f
MattMcPartlon <mmcpartlon@uchicago.edu>
2020-08-14 15:02:49 -0500

    first commit

---
+++ assignment.py
@@ -0,0 +1,12 @@
+"""
+
+12 points.
+
+Create the data structure from theory questoin 2, part c.
+
+at a minimum, your data structure should support is_valid(x : int),
+and add_range(lo, hi).
+
+You may use/modify the red black tree code provided as needed.
+
+"""
