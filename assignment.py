e142960a752ddb738f4416c83e51bba0ca8826fa
MattMcPartlon <35821457+MattMcPartlon@users.noreply.github.com>
2021-04-13 19:50:30 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@
 
 12 points.
 
-Create the data structure from theory questoin 2, part c.
+Create the data structure from theory question 1, part b.
 
 at a minimum, your data structure should support is_valid(x : int),
 and add_range(lo, hi).
